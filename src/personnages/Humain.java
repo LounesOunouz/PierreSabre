83072dff6c334d9c9ac939756b9ef65c13789217
Lounes Ounouz <nzl5176a@ensfsi.onmicrosoft.com>
2025-04-03 11:13:05 +0200

    les humains ont des connaissaances

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -4,6 +4,9 @@ public class Humain {
 	private String nom;
 	private String boissonPrefere;
 	private int quantiteArgent;
+	protected int nbConnaissance = 0;
+	private final int nbmh = 3;
+	protected Humain[] memoire = new Humain[nbmh];
 
 	public Humain(String nom, String boissonPrefere, int quantiteArgent) {
 		this.nom = nom;
@@ -56,6 +59,37 @@ public class Humain {
 					+ prix + " sous");
 		}
 
+	}
+
+	public void faireConnaissanceAvec(Humain autreHumain) {
+		direBonjour();
+		autreHumain.repondre(this);
+		memoriser(autreHumain);
+	}
+
+	public void repondre(Humain autrehumain) {
+		direBonjour();
+		memoriser(autrehumain);
+	}
+
+	public void memoriser(Humain autrehumain2) {
+		if (nbConnaissance < nbmh) {
+			memoire[nbConnaissance] = autrehumain2;
+			nbConnaissance++;
+		} else {
+			for (int i = 1; i < nbmh; i++) {
+				memoire[i - 1] = memoire[i];
+			}
+			memoire[nbmh - 1] = autrehumain2;
+		}
+	}
+
+	public void listerConnaissance() {
+		System.out.print("(" + getNom() + ")-Je connais beaucoup de monde dont:");
+		for (int i = 0; i < nbConnaissance; i++) {
+			System.out.print(memoire[i].getNom() + " ");
+		}
+		System.out.println(" ");
 	}
 
 }
